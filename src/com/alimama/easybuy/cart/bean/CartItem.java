8286f3d72e7cccc914324058d95c888deaf29311
1352348805 <1352348805@qq.com>
2020-07-16 20:04:35 +0800

    购物车

---
+++ src/com/alimama/easybuy/cart/bean/CartItem.java
@@ -0,0 +1,55 @@
+package com.alimama.easybuy.cart.bean;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Jun Xiao
+ * @date 2020/7/16 15:48
+ * 购物车单项数据
+ */
+public class CartItem {
+
+    //商品id
+    private Integer cid;
+
+    //商品单价
+    private BigDecimal price;
+
+    //商品数量
+    private Integer count;
+
+    private BigDecimal totalPrice;
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public BigDecimal getTotalPrice() {
+        BigDecimal bigDecimal = price.multiply(new BigDecimal(count.toString()));
+        return bigDecimal;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
--- test/com/alimama/easybuy/cart/CartTest.java
+++ test/com/alimama/easybuy/cart/CartTest.java
@@ -1,6 +1,12 @@
 package com.alimama.easybuy.cart;
 
+import com.alimama.easybuy.cart.bean.Cart;
+import com.alimama.easybuy.cart.bean.CartItem;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jun Xiao
@@ -10,8 +16,23 @@ public class CartTest {
 
     @Test
     public void f1() {
-        System.out.println(1);
+        Cart cart = new Cart();
+
+        CartItem cartItem = new CartItem();
+        cartItem.setPrice(new BigDecimal("12.5"));
+        cartItem.setCount(5);
+
+        CartItem cartItem1 = new CartItem();
+        cartItem1.setPrice(new BigDecimal("121.5"));
+        cartItem1.setCount(2);
+        System.out.println(cartItem.getTotalPrice());
+
+        List<CartItem> cartItems = new ArrayList<>();
 
 
+        cartItems.add(cartItem);
+        cartItems.add(cartItem1);
+        cart.setCartItems(cartItems);
+        System.out.println(cart.getTotalPrice());
     }
 }
