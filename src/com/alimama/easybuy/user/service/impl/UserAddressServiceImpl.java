52e21bc408c8bcbbb77861e8099e2f6f2e1b8225
1352348805 <1352348805@qq.com>
2020-07-17 19:43:52 +0800

    购物车

--- src/com/alimama/easybuy/user/service/impl/UserAddressServiceImpl.java
+++ src/com/alimama/easybuy/user/service/impl/UserAddressServiceImpl.java
@@ -4,6 +4,7 @@ import com.alimama.easybuy.user.bean.UserAddress;
 import com.alimama.easybuy.user.dao.UserAddressDao;
 import com.alimama.easybuy.user.dao.impl.UserAddressDaoImpl;
 import com.alimama.easybuy.user.service.UserAddressService;
+import com.alimama.easybuy.util.DatabaseUtil;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,95 +14,95 @@ import java.util.List;
  * @author Jun Xiao
  * @date 2020/7/17 16:22
  */
-public class UserAddressServiceImpl  {
+public class UserAddressServiceImpl implements UserAddressService {
 
-//
-//    @Override
-//    public List<UserAddress> getAddressByUserId(Integer uid) {
-//        List<UserAddress> userAddressList = null;
-//        Connection con = null;
-//        try {
-//            con = DBPool.getConnection();
-//            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
-//            userAddressList = userAddressDao.getAddressByUserId(uid);
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } finally {
-//            try {
-//                if (con != null) {
-//                    con.close();
-//                }
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-//        return userAddressList;
-//    }
-//
-//    @Override
-//    public Integer insertAddress(UserAddress userAddress) {
-//        Integer i = 0;
-//        Connection con = null;
-//        try {
-//            con = DBPool.getConnection();
-//            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
-//            i = userAddressDao.insertAddress(userAddress);
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } finally {
-//            try {
-//                if (con != null) {
-//                    con.close();
-//                }
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-//        return i;
-//    }
-//
-//    @Override
-//    public Integer modifyAddress(UserAddress userAddress) {
-//        Integer i = 0;
-//        Connection con = null;
-//        try {
-//            con = DBPool.getConnection();
-//            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
-//            i = userAddressDao.modifyAddress(userAddress);
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } finally {
-//            try {
-//                if (con != null) {
-//                    con.close();
-//                }
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-//        return i;
-//    }
-//
-//    @Override
-//    public Integer delAddress(Integer id) {
-//        Integer i = 0;
-//        Connection con = null;
-//        try {
-//            con = DBPool.getConnection();
-//            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
-//            i = userAddressDao.delAddress(id);
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        } finally {
-//            try {
-//                if (con != null) {
-//                    con.close();
-//                }
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-//        return i;
-//    }
+
+    @Override
+    public List<UserAddress> getAddressByUserId(Integer uid) {
+        List<UserAddress> userAddressList = null;
+        Connection con = null;
+        try {
+            con = DatabaseUtil.getConnection();
+            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
+            userAddressList = userAddressDao.getAddressByUserId(uid);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return userAddressList;
+    }
+
+    @Override
+    public Integer insertAddress(UserAddress userAddress) {
+        Integer i = 0;
+        Connection con = null;
+        try {
+            con = DatabaseUtil.getConnection();
+            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
+            i = userAddressDao.insertAddress(userAddress);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return i;
+    }
+
+    @Override
+    public Integer modifyAddress(UserAddress userAddress) {
+        Integer i = 0;
+        Connection con = null;
+        try {
+            con = DatabaseUtil.getConnection();
+            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
+            i = userAddressDao.modifyAddress(userAddress);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return i;
+    }
+
+    @Override
+    public Integer delAddress(Integer id) {
+        Integer i = 0;
+        Connection con = null;
+        try {
+            con = DatabaseUtil.getConnection();
+            UserAddressDao userAddressDao = new UserAddressDaoImpl(con);
+            i = userAddressDao.delAddress(id);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return i;
+    }
 
 }
