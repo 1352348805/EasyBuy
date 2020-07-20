238b0adaaf1047b647c731be18dc1e54dc73c543
1352348805 <1352348805@qq.com>
2020-07-20 10:28:10 +0800

    支付宝接口接入

---
+++ src/com/alimama/easybuy/order/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.alimama.easybuy.order.service;
+
+/**
+ * @author asuk
+ * @date 2020/7/20 0:13
+ */
+public interface OrderService {
+
+}
---
+++ src/com/alimama/easybuy/order/servlet/PayServlet.java
@@ -0,0 +1,32 @@
+package com.alimama.easybuy.order.servlet;
+
+import com.alimama.easybuy.order.service.impl.OrderServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+/**
+ * @author asuk
+ * @date 2020/7/19 23:22
+ */
+@WebServlet("/pay")
+public class PayServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OrderServiceImpl orderService = new OrderServiceImpl();
+
+        String result = orderService.payOrder(UUID.randomUUID().toString(), "50000", "xj", "ttt");
+        PrintWriter out = resp.getWriter();
+        out.print(result);
+        out.flush();
+        out.close();
+
+    }
+}
