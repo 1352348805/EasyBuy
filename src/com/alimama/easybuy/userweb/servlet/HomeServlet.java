25b6bd7636aa3af4e4ae32d5849100129372969e
1352348805 <1352348805@qq.com>
2020-07-20 19:22:24 +0800

    小更新

---
+++ src/com/alimama/easybuy/userweb/servlet/HomeServlet.java
@@ -0,0 +1,24 @@
+package com.alimama.easybuy.userweb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author asuk
+ * @date 2020/7/20 18:38
+ */
+@WebServlet("/Home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if ("index".equals(action)) {
+            req.getRequestDispatcher("/WEB-INF/page/userweb/home.jsp").forward(req,resp);
+        }
+    }
+}
